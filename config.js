e5debaef090c26fd40bcd700386fabcc1c2ecd40
Jisan rahaman <134159310+jisan2233@users.noreply.github.com>
2023-05-21 10:17:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['+917478603803', 'woner', true],
+  ['+917478603803', 'iconic lx shan', true], 
   [''] 
 ] //Number of owners
 
